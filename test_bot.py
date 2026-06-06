5ff1c3204988e8adaa283d10b4a6ffd3982c975d
Myahmyahmeow-cat <jeffreylilin@gmail.com>
2026-06-06 18:05:20 -0400

    final submission: clean repo

--- test_bot.py
+++
@@ -1,6 +0,0 @@
-import sys, traceback
-sys.argv = ['bot', 'https://tavus.daily.co/ca83dc804ff4d479', 'ca83dc804ff4d479']
-try:
-    exec(open('conversation_bot.py').read().replace("if __name__ == \"__main__\":", "if True:"))
-except Exception as e:
-    traceback.print_exc()
